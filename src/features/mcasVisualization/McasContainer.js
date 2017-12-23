1338d16daf95e058a26006443adb51aaabb88aed
commanderking <jek582@mail.harvard.edu>
2017-12-22 22:44:05 -0500

    add single graph comparison

--- src/features/mcasVisualization/McasContainer.js
+++ src/features/mcasVisualization/McasContainer.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { VictoryChart, VictoryBar, VictoryAxis, VictoryLabel } from 'victory';
+import { VictoryChart, VictoryBar, VictoryAxis, VictoryLabel, VictoryGroup, VictoryLegend, VictoryTheme } from 'victory';
 import { addSchoolAction, selectSchoolAction, deleteSchoolAction } from './mcasActions';
 import { mcasData } from './mcasData';
 import VirtualizedSelect from 'react-virtualized-select';
@@ -17,6 +17,19 @@ const categories = ['Exceeding', 'Meeting', 'Partially Meeting', 'Not Meeting'];
 const mcasDataConstants = {
   SCHOOL_NAME: 'School Name'
 }
+
+/**
+  * #aec6cf - pastel blue
+  * #b19cd9 - pastel purple
+  * #77dd77 - pastel green
+  * #ff6961 - pastel red
+  */
+const colors = [
+  "#aec6cf",
+  "#b19cd9",
+  "#77dd77",
+  "#ff6961"
+];
 
 export const graphConstants = {
   DOMAIN_PADDING: 40,
@@ -37,10 +50,23 @@ const mapCategoriesToRawDataValue = {
   [categories[3]] : 'NM %'
 };
 
-const formatDataForChart = school => {
+const formatDataForChart = (school, index) => {
+  console.log(colors[index]);
   return categories.map(category => {
-    return { x: category, y: school[mapCategoriesToRawDataValue[category]]}
+    return { x: category, y: school[mapCategoriesToRawDataValue[category]], fill: colors[index]}
   })
+}
+
+const formatDataForLegend = (allSchools, selectedSchoolIndexes) => {
+  return selectedSchoolIndexes.map((schoolIndex, index) => {
+    return {
+      name: allSchools[schoolIndex][mcasDataConstants.SCHOOL_NAME].split(' - ')[1],
+      symbol: {
+        fill: colors[index],
+        type: 'star'
+      }
+    }
+  });
 }
 
 const parseSchoolName = (schoolNameWithDistrict) => {
@@ -58,72 +84,118 @@ const getSchoolNames = (allSchools) => {
   return data;
 }
 
-export const UnwrappedMcasContainer = ({ allSchools, selectedSchoolIndexes, dropdownSchoolIndex, addSchoolClick, selectSchool, deleteSchool }) => (
-  <div>
-    <div className="schoolSelectWrapper">
-      <VirtualizedSelect
-        options={getSchoolNames(allSchools)}
-        onChange={(selectValue) => {
-          if (selectValue) {
-            selectSchool(selectValue.index);
-          }
-        }}
-        value={allSchools[dropdownSchoolIndex][mcasDataConstants.SCHOOL_NAME]}
-      />
-      <button onClick={ () => {
-        addSchoolClick(dropdownSchoolIndex);
-      }}>Add School</button>
-    </div>
-    {selectedSchoolIndexes.map((schoolIndex) => {
-      const schoolData = allSchools[schoolIndex];
-      return (
-        <div key={schoolData[mcasDataConstants.SCHOOL_NAME]} className='chartWrapper'>
-          <VictoryChart domainPadding={graphConstants.DOMAIN_PADDING} >
-            <VictoryLabel
-              text={parseSchoolName(schoolData[mcasDataConstants.SCHOOL_NAME])}
-              textAnchor="middle"
-              x={graphConstants.TITLE_X}
-              y={graphConstants.TITLE_Y}
-            />
-            <VictoryBar
-              data={formatDataForChart(schoolData)}
-              categories={{ x: categories}}
-              labels={(d) => `${d.y}%`}
-              style={{
-                data: {
-                  fill: graphConstants.BAR_FILL,
-                  stroke: graphConstants.BAR_STROKE,
-                  strokeWidth: graphConstants.BAR_STROKE_WIDTH
-                }
-              }}
-              />
-            <VictoryAxis
-              style={{
-                axisLabel: { padding: graphConstants.X_AXIS_LABEL_PADDING }
-              }}
-            />
-            <VictoryAxis dependentAxis
-              label={graphConstants.Y_AXIS_TEXT}
-              style={{
-                axisLabel: { padding: graphConstants.Y_AXIS_LABEL_PADDING }
-              }}
-              tickFormat={(t) => `${Math.round(t)}%`}
-            />
-          </VictoryChart>
-          <button
-            className="closeGraphButton"
-            onClick={() => {
-              deleteSchool(schoolIndex);
-            }}
-          >
-            X
-          </button>
-        </div>
-      )
-    })}
+export const UnwrappedMcasContainer = ({ allSchools, selectedSchoolIndexes, dropdownSchoolIndex, addSchoolClick, selectSchool, deleteSchool }) => {
+  const showLegend = selectedSchoolIndexes.length === 0 ? false : true;
+  return (
+    <div>
+      <div className="schoolSelectWrapper">
+        <VirtualizedSelect
+          options={getSchoolNames(allSchools)}
+          onChange={(selectValue) => {
+            if (selectValue) {
+              selectSchool(selectValue.index);
+            }
+          }}
+          value={allSchools[dropdownSchoolIndex][mcasDataConstants.SCHOOL_NAME]}
+        />
+        <button onClick={ () => {
+          addSchoolClick(dropdownSchoolIndex);
+        }}>Add School</button>
+      </div>
+      <div className='mcasChartWrapper'>
+        <VictoryChart
+          domainPadding={graphConstants.DOMAIN_PADDING}
+        >
+          { showLegend &&
+            <VictoryLegend x={125} y={10}
+              orientation="horizontal"
+              gutter={20}
+              padding={0}
+              rowGutter={-10}
+              itemsPerRow={1}
+              x={0}
+              y={0}
+              style={{ labels: { fontSize: 10 }}}
 
-  </div>
-);
+              data={formatDataForLegend(allSchools, selectedSchoolIndexes)}
+            />
+          }
+
+          <VictoryGroup offset={20}
+            padding={0}
+            colorScale={"qualitative"}
+            categories={{ x: categories}}
+          >
+            {selectedSchoolIndexes.map((schoolIndex, index) => {
+              return (
+                <VictoryBar
+                  labels={(d) => `${d.y}%`}
+                  style={{
+                    data: {
+                      width: 18,
+                      padding: 5
+                    },
+                    labels: {
+                      fontSize: 10
+                    }
+                  }}
+                  data={formatDataForChart(allSchools[schoolIndex], index)}
+                />
+              )
+            })}
+          </VictoryGroup>
+        </VictoryChart>
+      </div>
+      {selectedSchoolIndexes.map((schoolIndex, index) => {
+        const schoolData = allSchools[schoolIndex];
+        return (
+          <div key={schoolData[mcasDataConstants.SCHOOL_NAME]} className='chartWrapper'>
+            <VictoryChart domainPadding={graphConstants.DOMAIN_PADDING} >
+              <VictoryLabel
+                text={parseSchoolName(schoolData[mcasDataConstants.SCHOOL_NAME])}
+                textAnchor="middle"
+                x={graphConstants.TITLE_X}
+                y={graphConstants.TITLE_Y}
+              />
+              <VictoryBar
+                data={formatDataForChart(schoolData, index)}
+                categories={{ x: categories}}
+                labels={(d) => `${d.y}%`}
+                style={{
+                  data: {
+                    fill: graphConstants.BAR_FILL,
+                    stroke: graphConstants.BAR_STROKE,
+                    strokeWidth: graphConstants.BAR_STROKE_WIDTH
+                  }
+                }}
+                />
+              <VictoryAxis
+                style={{
+                  axisLabel: { padding: graphConstants.X_AXIS_LABEL_PADDING }
+                }}
+              />
+              <VictoryAxis dependentAxis
+                label={graphConstants.Y_AXIS_TEXT}
+                style={{
+                  axisLabel: { padding: graphConstants.Y_AXIS_LABEL_PADDING }
+                }}
+                tickFormat={(t) => `${Math.round(t)}%`}
+              />
+            </VictoryChart>
+            <button
+              className="closeGraphButton"
+              onClick={() => {
+                deleteSchool(schoolIndex);
+              }}
+            >
+              X
+            </button>
+          </div>
+        )
+      })}
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   const { selectedSchoolIndexes, dropdownSchoolIndex } = state;
