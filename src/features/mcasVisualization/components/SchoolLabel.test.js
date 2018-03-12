f1a1cdb58d1b87be1b397651f874b5694b37627d
Jeffrey King <jek582@mail.harvard.edu>
2018-03-11 20:23:34 -0400

    add unit tests for School Label (#6)

---
+++ src/features/mcasVisualization/components/SchoolLabel.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { SchoolLabel } from "./SchoolLabel";
+
+describe("School Label", () => {
+  let mockProps = {
+    schoolName: "mock-school-name",
+    schoolCode: 12345,
+    deleteSchool: jest.fn(),
+    index: 1
+  };
+  let wrapper = shallow(<SchoolLabel {...mockProps} />);
+
+  it("renders correct structure", () => {
+    expect(wrapper.find(".schoolLabelWrapper")).toHaveLength(1);
+    expect(wrapper.find(".selectedSchoolsIcon")).toHaveLength(1);
+    expect(wrapper.find(".selectedSchoolsIcon")).toHaveLength(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("button calls deleteSchool prop function", () => {
+    const deleteSchoolButton = wrapper.find("button");
+    deleteSchoolButton.simulate("click");
+    expect(mockProps.deleteSchool).toHaveBeenCalledTimes(1);
+    expect(mockProps.deleteSchool).toHaveBeenCalledWith(mockProps.schoolCode);
+  });
+});
